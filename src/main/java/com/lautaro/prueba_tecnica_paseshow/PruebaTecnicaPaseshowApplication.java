d2a5de80c2b9a38590133b77bdd96aebc1e3ccf4
Lautaro Carrizo <laucarrizo332@gmail.com>
2025-07-22 11:42:04 -0300

    primer commit

---
+++ src/main/java/com/lautaro/prueba_tecnica_paseshow/PruebaTecnicaPaseshowApplication.java
@@ -0,0 +1,13 @@
+package com.lautaro.prueba_tecnica_paseshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaPaseshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaPaseshowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lautaro/prueba_tecnica_paseshow/PruebaTecnicaPaseshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lautaro.prueba_tecnica_paseshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaPaseshowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
