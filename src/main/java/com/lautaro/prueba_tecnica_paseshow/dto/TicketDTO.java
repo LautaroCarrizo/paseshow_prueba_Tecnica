c53248aa8bc4d17e0e374720981f7ebdfcbcae51
Lautaro Carrizo <laucarrizo332@gmail.com>
2025-07-23 12:27:28 -0300

    add ticket dto

--- src/main/java/com/lautaro/prueba_tecnica_paseshow/dto/TicketDTO.java
+++ src/main/java/com/lautaro/prueba_tecnica_paseshow/dto/TicketDTO.java
@@ -1,4 +1,14 @@
 package com.lautaro.prueba_tecnica_paseshow.dto;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class TicketDTO {
+    private Long id;
+    private String evento;
+    private Long costo;
+    private Date fechaVigencia;
+    private Long clienteId;
 }
