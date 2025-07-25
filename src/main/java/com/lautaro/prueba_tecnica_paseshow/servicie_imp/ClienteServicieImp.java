d95017c1fa9b1e15ceecb95533f65ce537d6d366
Lautaro Carrizo <laucarrizo332@gmail.com>
2025-07-25 13:48:44 -0300

    implementacion de servicios y correciones

---
+++ src/main/java/com/lautaro/prueba_tecnica_paseshow/servicie_imp/ClienteServicieImp.java
@@ -0,0 +1,67 @@
+package com.lautaro.prueba_tecnica_paseshow.servicie_imp;
+
+import com.lautaro.prueba_tecnica_paseshow.dto.ClienteDTO;
+import com.lautaro.prueba_tecnica_paseshow.model.Cliente;
+import com.lautaro.prueba_tecnica_paseshow.servicies.ClienteServicie;
+import com.lautaro.prueba_tecnica_paseshow.repository.ClienteRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ClienteServicieImp implements ClienteServicie {
+    @Autowired
+    private ClienteRepository repo_cliente;
+    @Autowired
+    private ModelMapper modelMapper;
+    private ClienteValidacionExternaImp validacionExterna = new ClienteValidacionExternaImp();
+
+    @Override
+    public List<ClienteDTO> getAllClientesServicie() {
+        List<Cliente> clientesModel = repo_cliente.findAll();
+        return clientesModel.stream().map(cliente -> modelMapper.map(cliente, ClienteDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public ClienteDTO getClienteServicie(Long id) {
+        Cliente cliente = repo_cliente.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente no encontrado con ID: " + id));
+        return modelMapper.map(cliente, ClienteDTO.class);
+
+    }
+
+    @Override
+    public ClienteDTO postClienteServicie(ClienteDTO dto) {
+        if (repo_cliente.existsByDni(dto.getDni())) {
+            throw new RuntimeException("Ya existe un cliente con ese dni");
+        }
+        boolean validado = validacionExterna.validarClienteEndPointPaseshow(dto.getNombre(), dto.getApellido());
+        if (!validado) {
+            throw new RuntimeException("Cliente rechazado por validacion externa");
+        }
+        Cliente cliente = modelMapper.map(dto, Cliente.class);
+        Cliente clienteGuardado = repo_cliente.save(cliente);
+        return modelMapper.map(clienteGuardado, ClienteDTO.class);
+    }
+
+    @Override
+    public ClienteDTO updateClienteServicie(ClienteDTO dto, Long id) {
+        Cliente cliente = repo_cliente.findById(id).orElseThrow(() -> new RuntimeException("Cliente no encontrado con ID: " + id));
+        cliente.setNombre(dto.getNombre());
+        cliente.setApellido(dto.getApellido());
+        cliente.setDni(dto.getDni());
+        cliente.setFechaNacimiento(dto.getFechaNacimiento());
+        Cliente actualizado = repo_cliente.save(cliente);
+        return modelMapper.map(actualizado, ClienteDTO.class);
+    }
+
+    @Override
+    public void deleteClienteServicie(Long id) {
+        Cliente cliente = repo_cliente.findById(id).orElseThrow(() -> new RuntimeException("Cliente no encontrado con ID: " + id));
+        repo_cliente.delete(cliente);
+    }
+}
---
+++ src/main/java/com/lautaro/prueba_tecnica_paseshow/servicie_imp/TicketServicieImp.java
@@ -0,0 +1,66 @@
+package com.lautaro.prueba_tecnica_paseshow.servicie_imp;
+import com.lautaro.prueba_tecnica_paseshow.dto.ClienteDTO;
+import com.lautaro.prueba_tecnica_paseshow.dto.TicketDTO;
+import com.lautaro.prueba_tecnica_paseshow.model.Cliente;
+import com.lautaro.prueba_tecnica_paseshow.model.Ticket;
+import com.lautaro.prueba_tecnica_paseshow.repository.ClienteRepository;
+import com.lautaro.prueba_tecnica_paseshow.repository.TicketRepository;
+import com.lautaro.prueba_tecnica_paseshow.servicies.TicketServicie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TicketServicieImp implements TicketServicie {
+    @Autowired
+    private TicketRepository repo_ticket;
+    @Autowired
+    private ClienteRepository repo_cliente;
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    @Override
+    public List<TicketDTO> getAllTicketsServicie() {
+        List<Ticket> ticketModel = repo_ticket.findAll();
+        return ticketModel.stream().map(ticket -> modelMapper.map(ticket, TicketDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public TicketDTO getTicketServicie(Long id) {
+        Ticket ticket = repo_ticket.findById(id)
+                .orElseThrow(() -> new RuntimeException("Ticket no encontrado con ID: " + id));
+        return modelMapper.map(ticket, TicketDTO.class);
+    }
+
+    @Override
+    public TicketDTO postTicketServicie(TicketDTO dto) {
+        Cliente cliente = repo_cliente.findById(dto.getId()).orElseThrow(() -> new RuntimeException("Cliente no encontrado con ID: " + dto.getId()));
+        if (cliente.getTickets().isEmpty()) {
+            throw new RuntimeException("El cliente no tiene tickets");
+        }
+        Ticket ticket = modelMapper.map(dto, Ticket.class);
+        Ticket ticketGuardado = repo_ticket.save(ticket);
+        return modelMapper.map(ticketGuardado, TicketDTO.class);
+    }
+
+    @Override
+    public TicketDTO updateTicketServicie(TicketDTO dto, Long id) {
+        Ticket ticket = repo_ticket.findById(id).orElseThrow(() -> new RuntimeException("Cliente no encontrado con ID: " + id));
+        ticket.setCosto(dto.getCosto());
+        ticket.setEvento(dto.getEvento());
+        ticket.setFechaVigencia(dto.getFechaVigencia());
+        Ticket actualizado = repo_ticket.save(ticket);
+        return modelMapper.map(actualizado, TicketDTO.class);
+    }
+
+    @Override
+    public void deleteTicketServicie(Long id) {
+        Ticket ticket = repo_ticket.findById(id).orElseThrow(() -> new RuntimeException("Ticket no encontrado con ID: " + id));
+        repo_ticket.delete(ticket);
+    }
+}
