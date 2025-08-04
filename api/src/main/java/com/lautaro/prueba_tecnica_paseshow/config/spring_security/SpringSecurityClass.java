57d59b309ad103dd8622f337fcc67412a1e881c5
Lautaro Carrizo <laucarrizo332@gmail.com>
2025-08-04 20:41:26 -0300

    Modificando la seguridad para realizar peticiones del front

--- api/src/main/java/com/lautaro/prueba_tecnica_paseshow/config/spring_security/SpringSecurityClass.java
+++ api/src/main/java/com/lautaro/prueba_tecnica_paseshow/config/spring_security/SpringSecurityClass.java
@@ -2,6 +2,8 @@ package com.lautaro.prueba_tecnica_paseshow.config.spring_security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -9,10 +11,14 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecurityClass {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .httpBasic(httpBasic -> {
-                });
-        return http.build();
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 }
