ded0c96b90ffdfd5154afd06aa2268fa4bd21e29
Lautaro Carrizo <laucarrizo332@gmail.com>
2025-08-04 20:27:16 -0300

    modificando cors de nuevo

--- api/src/main/java/com/lautaro/prueba_tecnica_paseshow/config/cors/CorsConfig.java
+++ api/src/main/java/com/lautaro/prueba_tecnica_paseshow/config/cors/CorsConfig.java
@@ -13,9 +13,12 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") 
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); 
             }
         };
     }
 }
+
